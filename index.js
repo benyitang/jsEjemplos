b69758a5efac03e302450b3cf4ebdf4eb0d4e98f
valenNu3u <papucho04yt@gmail.com>
2024-03-25 15:23:24 -0300

    function4

--- index.js
+++ index.js
@@ -47,8 +47,31 @@ console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igua
 }
 
 }
+
 function ejercicio4(){
 
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+    'E', 'T'];
+
+let dni = document.querySelector("#dni").value;
+let letrauser = document
+
+
+
+if(dni > 0 && dni < 99999999) {
+let nletra = dni % 23;
+
+let letra = letras[nletra];
+
+if(letra == letrauser){
+    console.log("Las letras son iguales")
+
+
+    console.log("La letra de tu DNI es:  "+ letra);
+
+ }
+ else{
+    console.log("Es incorrecto")
+ }
 }
-
-
+}
