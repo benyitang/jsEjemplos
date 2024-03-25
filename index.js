b900bd5a47ac602c05c993b9507e96a197c81d50
valenNu3u <papucho04yt@gmail.com>
2024-03-25 14:26:37 -0300

    a

---
+++ index.js
@@ -0,0 +1,54 @@
+function ejercicio1() {
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+    for (let i = 0; i < meses.length; i++) {
+        console.log(meses[i]);
+    }
+
+}
+
+function ejercicio2() {
+    let valores = [true, 5, false, "hola", "adios",2];
+    
+if(valores[3].length < valores[4].length){
+    console.log(valores[0])
+    console.log("adios es la palabra con mas letras")
+ }
+else{
+    console.log(valores[2])
+    console.log("hola es la palabra con menos letras")
+ }
+console.log(valores[1]  +   valores[5])
+console.log(valores[1]  -   valores[5])
+console.log(valores[1]  *   valores[5])
+console.log(valores[1]  /   valores[5])
+}
+
+
+function ejercicio3(){
+
+    var numero1 = 5;
+    var numero2 = 8;
+
+    if(numero1<numero2) {
+console.log("numero1 no es mayor que numero2");
+ }
+
+if(numero2>0) {
+console.log("numero2 es positivo");
+ }
+
+if(numero1<0) {
+console.log("numero1 es negativo o distinto de cero");
+ }
+
+if(numero1 +1 <numero2) {
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+
+}
+function ejercicio4(){
+
+}
+
+
